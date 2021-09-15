7ffa8c3e61832671a083e115558d555c3fabe7f5
kovid <kovid@localhost>
2021-09-15 17:06:01 -0400

    First Commit

--- src/main/java/pageobject/feature1Page.java
+++ src/main/java/pageobject/feature1Page.java
@@ -21,10 +21,6 @@ public static Properties properties;
 
 /*Xpaths*/
 public static By btnSubmit = By.xpath("(//input[contains(@value, 'Google')])[2]");
-public static By searchBox = By.xpath("//input[@title='Search']");
-public static By defaultText = By.xpath("(//input[contains(@class,'gLFyf gsfi')])[1]");
-
-public static By defaultButton = By.xpath("(//input[contains(@class,'gNO89b')])[2]");
 
 //<XPATHS>
 
